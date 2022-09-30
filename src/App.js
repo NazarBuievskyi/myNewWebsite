46e40a3cb7e65a057dc136b76c6488a34e1084e3
Nazar <nbuievskyi@gmail.com>
2022-09-30 18:06:35 +0200

    Styled intro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+//Import Global style
+//Import Pages
 import {AboutUs} from "./pages/AboutUs";
+import {GlobalStyle} from "./components/GlobalStyle";
+
 
 function App() {
     return (
         <div className="App">
+            <GlobalStyle/>
             <AboutUs/>
         </div>
     );
